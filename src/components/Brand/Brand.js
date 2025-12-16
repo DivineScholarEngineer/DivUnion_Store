47bdf691e39a294af78afb4fe2fe35b6c81af18b
Divineseed Olorunfemi <60397755+DivineScholarEngineer@users.noreply.github.com>
2025-12-16 14:57:13 -0700

    Update brand header styling

--- src/components/Brand/Brand.js
+++ src/components/Brand/Brand.js
@@ -3,7 +3,7 @@ import { navigate } from 'gatsby';
 
 import * as styles from './Brand.module.css';
 
-const Brand = (props) => {
+const Brand = () => {
   return (
     <div
       className={styles.root}
